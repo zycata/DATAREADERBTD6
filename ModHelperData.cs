97f28ecf283d302be97ba50753aafd88ec4b7553
zycata <hyperconger@gmail.com>
2025-07-30 22:05:50 -0700

    Changed some readmes

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,8 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "DATAREADER";
 
-    public const string Description = "uhh soo i basically needed something that read game files stuff so i could make a project yeahhh";
+    public const string Description = "uhh soo i basically needed something that read game files stuff so i could make a project yeahhh, pretty much just writes to a file which then can be used by other progrmas." +
+        " The other project that works with it is: https://github.com/zycata/BTD6Machina";
 
     public const string RepoOwner = "zycata"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "DATAREADERBTD6"; // TODO add your repo name here, also in the download url in README.md
--- README.md
+++ README.md
@@ -11,6 +11,8 @@ Hi so this is a mod I made so my little "Ai" project can read data from the game
 
 It's pretty simple pretty much just reads the data from the game then sends it to a json file that can be read
 by another program. (in this case my Ai project). 
-if code looks messy I will change that soon^TM^
+if code looks messy I will change that soon^TM^, 
+Repository for the little "AI" project that uses this: https://github.com/zycata/BTD6Machina 
+
 
 [![Requires BTD6 Mod Helper](https://raw.githubusercontent.com/gurrenm3/BTD-Mod-Helper/master/banner.png)](https://github.com/gurrenm3/BTD-Mod-Helper#readme)
